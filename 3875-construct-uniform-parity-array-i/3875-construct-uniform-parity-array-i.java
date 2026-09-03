92fd1e4743b3508cbcda55ad17871f797f86e4eb
Nitin-Tiwari488 <nitintiwari0058@gmail.com>
2026-09-03 12:16:59 +0530

    Time: 0 ms (100%), Space: 45.4 MB (16.17%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // Only the observation in this question is when even and odd
+        // both numbers are present we can change them only odd number using parity like..
+        // If you substract any even number by odd number you will always find odd number 
+        // so nums1 can be converted to nums2 
+        return true;
+    }
+}
