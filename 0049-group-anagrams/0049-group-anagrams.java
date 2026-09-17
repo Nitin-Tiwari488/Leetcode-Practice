d1c07ff067c54a0a9a2e51d84df5df8817d86e57
Nitin-Tiwari488 <nitintiwari0058@gmail.com>
2026-09-17 20:26:31 +0530

    Time: 7 ms (71.86%), Space: 49.5 MB (72.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        
         int n = strs.length;
-
         HashMap<String , List<String>> mp = new HashMap<>();
-
         for(int i=0;i<n;i++){
+
             String s = strs[i];
-            
             char[] ch = s.toCharArray();
             Arrays.sort(ch);
             String key = new String(ch);
-
+            
             if(!mp.containsKey(key)){
                 mp.put(key , new ArrayList<>());
             }
+            // Here we are not using else because if we don't found key then only
+            // we will create new section with null value if exist then direct add s
             mp.get(key).add(s);
         }
         return new ArrayList<>(mp.values());
